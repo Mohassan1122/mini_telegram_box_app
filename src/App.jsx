c9219893e1b324390b6c9ace16f81f57cde85e78
mohammed hassan <mohammedhassanage26@gmail.com>
2024-09-13 17:02:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [response, setResponse] = useState('');
+
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center text-white bg-slate-950 justify-center no-scroll p-6">
+        <h1 className="text-3xl text-amber-600 font-bold mb-8  responsive-text">Welcome</h1>
+
+        <div className="flex flex-col items-center text-center justify-center font-extrabold p-3 space-y-4 max-w-lg">
+          <p className="responsive-text">Join us on this exciting journey.<br /> Mine <br /><span className='md:text-lg text-xxs'> Multi-Layered Autonomous Blockcahain Utility Token</span>  <br /> $MCT <br /> Join the evolution</p>
+        </div>
+
+        <button
+          onClick=""
+          className="outline outline-amber-600  text-amber-600 my-20 py-2 px-16 rounded-full hover:bg-gray-800 "
+        > <span className='text-xl px-2 font-extrabold yeseva-one-regular'>
+            Start
+          </span>
+
+        </button>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
+
