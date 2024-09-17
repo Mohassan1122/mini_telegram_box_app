e08b97b9440c0f6b2ddfe251b0e1294f46ebfd6a
Multi-Layered Autonomous Blockchain <mlabtoken@gmail.com>
2024-09-17 12:07:01 +0100

    Update and rename Leadership.jsx to Leaderboard.jsx

---
+++ src/pages/Leaderboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Leaderboard() {
+    return (
+        <div>
+                <div className="flex flex-col items-center text-center justify-center font-extrabold p-3 space-y-4 max-w-lg">
+                   <p className="responsive-text">Coming up Soon... </p>
+                </div>
+        </div>
+    )
+}
+
+export default Leaderboard
